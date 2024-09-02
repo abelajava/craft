2fb0d4a0fc648e9d1ed21e448ae0530b071ce465
“abelajava” <abelasratu@icloud.com>
2024-09-01 23:08:35 -0400

    Updated initial

---
+++ java/src/java/com/SecondProgram.java
@@ -0,0 +1,13 @@
+package java.com;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 2;
+		int[] nums = new int[5];
+		nums[3] = 944;
+		System.out.println("This is program of sum: " + num + nums);
+	}
+
+}
